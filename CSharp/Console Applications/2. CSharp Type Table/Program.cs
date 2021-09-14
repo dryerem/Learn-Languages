bdbc63f50d6ef4b59769b32858758dc8fcb5025b
Denis Eremenko <mamayma8@gmail.com>
2021-09-15 02:02:52 +0300

    I started to learn CSharp and git

---
+++ CSharp/Console Applications/2. CSharp Type Table/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Sharp_Type_Table
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type\tCTS Type\tSize\tRange of values");
+            Console.WriteLine("bool\t" + typeof(bool) + "\t" + sizeof(bool) + "\t" + bool.TrueString + "..." + bool.FalseString);
+            Console.WriteLine("byte\t" + typeof(byte) + "\t" + sizeof(byte) + "\t" + byte.MinValue + "..." + byte.MaxValue);
+            Console.WriteLine("sbyte\t" + typeof(sbyte) + "\t" + sizeof(sbyte) + "\t" + sbyte.MinValue + "..." + sbyte.MaxValue);
+            Console.WriteLine("short\t" + typeof(short) + "\t" + sizeof(short) + "\t" + short.MinValue + "..." + short.MaxValue);
+            Console.WriteLine("ushort\t" + typeof(ushort) + "\t" + sizeof(ushort) + "\t" + ushort.MinValue + "..." + ushort.MaxValue);
+            Console.WriteLine("int\t" + typeof(int) + "\t" + sizeof(int) + "\t" + "-2147483648...-2147483647");
+            Console.WriteLine("uint\t" + typeof(uint) + "\t" + sizeof(uint) + "\t" + uint.MinValue + "..." + uint.MaxValue);
+            Console.WriteLine("long\t" + typeof(long) + "\t" + sizeof(long) + "\t" + long.MinValue + "..." + long.MaxValue);
+            Console.WriteLine("ulong\t" + typeof(ulong) + "\t" + sizeof(ulong) + "\t" + ulong.MinValue + "..." + ulong.MaxValue);
+            Console.WriteLine("decimal\t" + typeof(decimal) + "\t" + sizeof(decimal) + "\t" + decimal.MinValue + "..." + decimal.MaxValue);
+            Console.WriteLine("float\t" + typeof(float) + "\t" + sizeof(float) + "\t" + float.MinValue + "..." + float.MaxValue);
+            Console.WriteLine("double\t" + typeof(double) + "\t" + sizeof(double) + "\t" + double.MinValue + "..." + double.MaxValue);
+            Console.WriteLine("string\t" + typeof(string) + "\t" + "inf\t" + "empty...inf");
+            Console.WriteLine("char\t" + typeof(char) + "\t" + sizeof(char) + "\t" + "Unicode symbol");
+        }
+    }
+}
---
+++ CSharp/Console Applications/3. Console Calculator/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Globalization;
+
+namespace Console_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello! Welcome to simple console calculator!");
+
+            double a;
+            double b;
+            Console.Write("Enter a first number: ");
+            string firstNumber = Console.ReadLine();
+            if (!double.TryParse(firstNumber, NumberStyles.AllowDecimalPoint, getSeparator(firstNumber), out a))
+            {
+                Console.WriteLine("Error! Uncorrect number!");
+                Environment.Exit(1);
+            }
+            Console.Write("Enter a second number: ");
+            string secondNumber = Console.ReadLine();
+            if (!double.TryParse(secondNumber, NumberStyles.AllowDecimalPoint, getSeparator(secondNumber), out b))
+            {
+                Console.WriteLine("Error! Uncorrect number!");
+                Environment.Exit(1);
+            }
+            Console.Write("Enter a sign: ");
+            string sign = Console.ReadLine();
+    
+            Console.Write("Result: ");
+            switch (sign)
+            {
+                case "+":
+                    Console.Write(a + b);
+                    break;
+                case "-":
+                    Console.Write(a - b);
+                    break;
+                case "*":
+                    Console.Write(a * b);
+                    break;
+                case "/":
+                    Console.Write(a / b);
+                    break;
+                default:
+                    Console.WriteLine("Uncorrect sign!");
+                    break;
+            }
+            Console.WriteLine("");
+        }
+
+        static NumberFormatInfo getSeparator(string input)
+        {
+            NumberFormatInfo format = new NumberFormatInfo();
+            if (input.Contains(".")) {
+                format.NumberDecimalSeparator = ".";
+            }
+            else {
+                format.NumberDecimalSeparator = ",";
+            }
+            return format;
+        }
+    }
+}
