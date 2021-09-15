1bf6343c6c142047fd5d0df01a42e5793a913327
Denis Eremenko <mamayma8@gmail.com>
2021-09-15 19:13:53 +0300

    This program calculates the average of two numbers
    I learned the method "TryParse"

---
+++ CSharp/Console Applications/4. Average/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Average
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This program calculates the average of two numbers");
+
+            Console.Write("Input the first number: ");
+            double a;
+            string firstNumber = Console.ReadLine();
+            if (!double.TryParse(firstNumber, out a))
+            {
+                Console.WriteLine("Error! Please try again");
+                Environment.Exit(1);
+            }
+            
+            Console.Write("Input the second number: ");
+            double b;
+            string secondNumber = Console.ReadLine();
+            if (!double.TryParse(secondNumber, out b))
+            {
+                Console.WriteLine("Error! Please try again");
+                Environment.Exit(1);
+            }
+
+            double result = (a + b) / 2.0;
+            Console.WriteLine("Result: " + result);
+        }
+    }
+}
