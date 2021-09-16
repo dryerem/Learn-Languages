4e61e8c07d1c2ff08cec5d7ba4739c06ff925b8a
Denis Eremenko <mamayma8@gmail.com>
2021-09-17 01:46:42 +0300

    Check for an even number simple program

---
+++ CSharp/Console Applications/7. Check for an even number/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Check_for_an_even_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Check for an even number program");
+
+	    int number;
+	    Console.Write("Please, enter the number: ");
+	    if (!int.TryParse(Console.ReadLine(), out number))
+	    {
+		Console.WriteLine("Error! Uncorrect input number");
+		Environment.Exit(1);
+	    }
+
+	    if (number % 2 == 0)
+	    {
+		Console.WriteLine("This number is even!");
+	    }
+	    else 
+	    {
+		Console.WriteLine("This number not even!");
+	    }
+        }
+    }
+}
