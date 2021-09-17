9532d0599dc66bb4ad6993b10209e7483c4ee5b3
Denis Eremenko <mamayma8@gmail.com>
2021-09-18 02:44:49 +0300

    This program draw rectangle

---
+++ CSharp/Console Applications/9. Draw rectangle/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Draw_rectangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Draw triangle");
+
+	    int height;
+	    Console.Write("Input the height of the rectangle: ");
+	    if (!int.TryParse(Console.ReadLine(), out height))
+	    {
+		Console.WriteLine("Incorrect input number, please, try again");
+		Environment.Exit(1);
+	    }
+
+	    int width;
+	    Console.Write("Input the width of the rectangle: ");
+	    if (!int.TryParse(Console.ReadLine(), out width))
+	    {
+		Console.WriteLine("Incorrect input number, please, try again");
+		Environment.Exit(1);
+	    }
+
+	    for (int recHeight = 0; recHeight < height; recHeight++)
+	    {
+		for (int recWidth = 0; recWidth < width; recWidth++)
+		{
+		    Console.Write("#");
+		}
+		Console.WriteLine("#");
+	    }
+        }
+    }
+}
