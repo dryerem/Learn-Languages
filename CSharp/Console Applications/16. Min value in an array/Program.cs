9476e0606165a1392b85d699bb2ad93285ac2423
Denis Eremenko <mamayma8@gmail.com>
2021-09-20 00:57:13 +0300

    Find minimal value in an array

---
+++ CSharp/Console Applications/16. Min value in an array/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Reverse_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sum of even digits in an array");
+		
+	    Console.Write("Input the size array: ");
+	    int size = int.Parse(Console.ReadLine());
+
+	    int[] array = new int[size];
+	    for (int i = 0; i < size; i++)
+	    {
+		Console.Write("Array value [" + i + "]: ");
+		array[i] = int.Parse(Console.ReadLine());
+	    }
+
+	    Console.Write("Array: ");
+	    for (int i = 0; i < size; i++)
+		Console.Write(array[i] + " ");
+	    Console.WriteLine();
+
+	    int min = array[0]; 
+	    for (int i = 1; i < size; i++)
+		if (array[i] < min)
+		    min = array[i];
+	    Console.WriteLine("Minimal value: " + min);
+        }
+    }
+}
