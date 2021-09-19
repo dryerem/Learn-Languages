2d21bb4b361644b52dab7be8a57e9936ae4335ba
Denis Eremenko <mamayma8@gmail.com>
2021-09-20 00:49:13 +0300

    Sum of even digits in an array

---
+++ CSharp/Console Applications/15. Sum of even digits in an array/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Reverse_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Sum of even digits in an array");
+		
+	    Console.Write("Input the size array: ");
+	    int size = int.Parse(Console.ReadLine());
+
+	    int[] array = new int[size];
+	    for (int i = 0; i < size; i++)
+	    {
+		Console.Write("Array value [" + i + "]: ");
+		array[i] = int.Parse(Console.ReadLine());
+	    }
+
+	    Console.Write("Array: ");
+	    for (int i = 0; i < size; i++)
+		Console.Write(array[i] + " ");
+	    Console.WriteLine();
+
+	    int s = 0; 
+	    for (int i = 0; i < size; i++)
+	    {
+		if (array[i] % 2 == 0)
+		    s += array[i];
+	    }
+	    Console.WriteLine("Sum: " + s);
+        }
+    }
+}
