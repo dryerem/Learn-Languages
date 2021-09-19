b04086543d7d617f150254d88518ac515cea8560
Denis Eremenko <mamayma8@gmail.com>
2021-09-19 15:48:47 +0300

    I'm trying the ternary operator

---
+++ CSharp/Console Applications/11. Ternaty operator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ternaty_operator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ternary operator");
+
+	    bool access = false;
+	    string password = "qwerty";
+		
+	    Console.WriteLine("[if-else]");
+	    Console.Write("Input your password: ");
+	    if (Console.ReadLine() == password)
+	    	access = true;
+	    Console.WriteLine("Access: " + access + "\n");
+
+	    Console.WriteLine("[Ternary operator]");
+	    Console.Write("Input your password: ");
+	    bool ternary_access = Console.ReadLine() == password ? true : false;
+	    Console.WriteLine("Access: " + ternary_access);
+        }
+    }
+}
