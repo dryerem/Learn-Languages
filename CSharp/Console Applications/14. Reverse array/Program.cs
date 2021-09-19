ad28ccaabc06e2b9dceb90d8c7bd2475cc4682ae
Denis Eremenko <mamayma8@gmail.com>
2021-09-20 00:42:44 +0300

    Print the array in reverse order

---
+++ CSharp/Console Applications/14. Reverse array/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Reverse_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Print the array in reverse order");
+		
+	    Console.Write("Input the size array: ");
+	    int size = int.Parse(Console.ReadLine());
+
+	    int[] array = new int[size];
+	    for (int i = 0; i < size; i++)
+	    {
+		Console.Write("Array value [" + i + "]: ");
+		array[i] = int.Parse(Console.ReadLine());
+	    }
+
+	    Console.Write("Array: ");
+	    for (int i = 0; i < size; i++)
+		Console.Write(array[i] + " ");
+	    Console.WriteLine();
+
+	    Console.Write("Reverse: ");
+	    for (int i = size - 1; i >= 0; i--)
+		Console.Write(array[i] + " ");
+	    Console.WriteLine();
+        }
+    }
+}
