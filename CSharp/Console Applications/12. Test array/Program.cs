c40b032f6aa75e3820c2cef557483f5bd33b0475
Denis Eremenko <mamayma8@gmail.com>
2021-09-19 16:07:44 +0300

    create array syntax

---
+++ CSharp/Console Applications/12. Test array/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Test_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test array");
+		
+	    // create array
+	    int[] array = new int[5];
+
+	    array[1] = 3;
+	    array[4] = 5;
+	
+	    /* index out of range
+	    array[5] = 1;
+	    array[-1] = 5;
+	    */
+
+	    Console.WriteLine("Array[1] = " + array[1]);
+	    Console.WriteLine("Array[4] = " + array[4]);
+        }
+    }
+}
