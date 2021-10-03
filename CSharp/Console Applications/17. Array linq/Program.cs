8fb99c9fcdf4a9b9088e30eeca85064246706353
Denis Eremenko <mamayma8@gmail.com>
2021-10-03 19:23:52 +0300

    update

---
+++ CSharp/Console Applications/17. Array linq/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace Array_linq
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+
+            Console.WriteLine($"Max element in array: {Array.Max()}");
+            Console.WriteLine($"Min element in array: {Array.Min()}");
+            Console.WriteLine($"Sum of array: {Array.Sum()}");
+        }
+    }
+}
