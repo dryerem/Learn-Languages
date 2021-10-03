c238538692bb40650cfc89c48f2038b202ae0965
Denis Eremenko <mamayma8@gmail.com>
2021-10-03 17:36:15 +0300

    Familiarity with the syntax for creating two-dimensional rectangular arrays

---
+++ CSharp/Console Applications/19. Two-dimensional rectangular array/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Two_dimensional_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArray = new int[4, 2]; // row, column
+
+	    for (int row = 0; row < 4; row++)
+	    {
+		for (int column = 0; column < 2; column++)
+		{
+		    Console.Write($"row: {row}, column: {column} : ");
+		    int val = int.Parse(Console.ReadLine());
+		    myArray[row, column] = val;
+		}			
+	    }
+
+	    Console.WriteLine("myArray: ");
+	    for (int row = 0; row < 4; row++)
+	    {
+		for (int column = 0; column < 2; column++)
+		{
+		    Console.Write(myArray[row, column] + "\t");
+		}
+		Console.WriteLine();
+	    }
+        }
+    }
+}
