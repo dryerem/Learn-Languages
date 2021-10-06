4c3c0083daf91def9050b65c0e153b3b2a998c5a
Denis Eremenko <mamayma8@gmail.com>
2021-10-06 14:19:46 +0300

    3d arrays

---
+++ CSharp/Console Applications/21. 3D Arrays (трехмерные массивы)/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Arrays__трехмерные_массивы_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,,] myArray = new int[4, 3, 5];
+
+	    	Random rand = new Random();
+			int x, y, z;
+	    	for (x = 0; x < 4; x++)
+	    	{
+				for (y = 0; y < 3; y++)
+				{
+		    		for (z = 0; z < 5; z++)
+		    		{
+						myArray[x, y, z] = rand.Next(255);
+		    		}
+				}
+	    	}
+
+	    	for (x = 0; x < 4; x++)
+	    	{
+				for (y = 0; y < 3; y++)
+				{
+		    		for (z = 0; z < 5; z++)
+		    		{
+						Console.WriteLine($"{{x: {x}, y: {y}, z: {z}}} - {myArray[x, y, z]}");
+		    		}
+				}
+				Console.WriteLine();
+	    	}
+        }
+    }
+}
