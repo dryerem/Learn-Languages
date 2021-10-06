8003d3ffb8438daf5a88ededb07bd25bd8881042
Denis Eremenko <mamayma8@gmail.com>
2021-10-06 14:56:27 +0300

    i started to learn wpf 

---
+++ CSharp/WPF/FirstLesson/FirstLesson/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FirstLesson
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        string _password = "qwerty";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonAuthClick(object sender, RoutedEventArgs e)
+        {
+            if (PasswordTextBox.Text != string.Empty)
+            {
+                if (PasswordTextBox.Text == _password)
+                {
+                    MessageBox.Show("Поздравляю! Вы верно ввели пароль!");
+                }
+                else
+                {
+                    MessageBox.Show("Неправильный пароль!");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Вы не ввели пароль!");
+            }
+        }
+    }
+}
