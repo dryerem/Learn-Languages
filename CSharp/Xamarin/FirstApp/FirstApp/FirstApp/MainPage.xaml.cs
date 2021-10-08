fc0bf5b88f4fafb1dc8190f3ac793235a1bb7053
Denis Eremenko <mamayma8@gmail.com>
2021-10-08 23:02:47 +0300

    i started to learn xamarin fraemwork
    This application should make it easier to tag students who are attending couples. It is planned to link it with google tables

---
+++ CSharp/Xamarin/FirstApp/FirstApp/FirstApp/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace FirstApp
+{
+    public partial class MainPage : ContentPage
+    {
+        string[] students = new string[] 
+        {
+            "Аветисян Ростислав",
+            "Борзухин Андрей",
+            "Генхель Иван",
+            "Дятлова Александра",
+            "Евдокимов Сергей",
+            "Егоров Кирилл",
+            "Еременко Денис",
+            "Каданчик Андрей",
+            "Корнинский Денис",
+            "Маматов Никита",
+            "Могучева Софья",
+            "Плиев Давид",
+            "Реута Максим",
+            "Семкив Виталий",
+            "Соловьев Виктор",
+            "Тебеньков Владислав",
+            "Ховрат Тимофей",
+            "Черняков Артем",
+            "Чикинев Алексей",
+            "Шубарина Дарья"
+        };
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+            StudentsList.ItemsSource = students;
+        }
+
+        private async void studentSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            await Navigation.PushAsync(new StudentPage());
+        }
+    }
+}
